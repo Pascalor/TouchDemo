58b5b98869b214c047e1cc6ba3ddbfa619c10893
JaiGhanekar <jaighanekar1224@gmail.com>
2015-09-26 20:39:40 -0500

    commit

--- TouchDemo.py
+++ TouchDemo.py
@@ -20,15 +20,17 @@ x = 100
 y = 140
 x1 = 50
 y1 = 90
-delay = 50
-breakpoint = 15
+delay = 25
+breakpoint = 7
 isPressed = True 
 center = ((x + x1) / 2 , (y + y1) / 2)
 
 count = 0 #This can be a list as well per pixle
 
+
 default = 0.00 # this is the scale value 
 scale = int()
+
 canvas = Canvas(root, height = canvas_height, width = canvas_width) #create the canvas 
 id1 = canvas.create_oval(x, y, x1, y1, tag = "Circle" , fill = "white", outline = "white")
 canvas.pack()
@@ -36,8 +38,7 @@ canvas.pack()
 while True : 
     #create a circle everyloop then make it grow 
     
-    ser = serial.Serial("/dev/cu.usbmodem1421", 9600)
-    trash = ser.readline()
+    ser = serial.Serial("/dev/tty.usbmodem0E20E711", 9600)
     value = ser.readline()
     #id1 = canvas.create_oval(x, y, x1, y1, tag = "Circle" , fill = "white", outline = "white")
     
@@ -45,20 +46,84 @@ while True :
     
     #ignore non string chars 
 
-    if '_______\r\n' in value :
-        value = value.replace('_______\r\n', '0')
-
-
-
-    if 'A\r\n' in value :
-        value = value.replace('A\r\n', '0')
-
-   
-    invalue = int(value.strip(''))
+    value = value.split()
+    try:   
+        invalue = int(value[0])
+    except IndexError:
+        continue    
     
+    print(value)
 
     #set some boundaries
     isActive = False
+     
+
+
+    if invalue <= 1 and isPressed == False : 
+        canvas.delete(id1)
+        canvas.pack()
+        canvas.update()
+        id1 = canvas.create_oval(x, y, x1, y1, tag = "Circle" , fill = "white", outline = "white")
+        scale = 0.00
+        default = 0.00
+        isActive = False 
+
+    elif invalue > 1 and invalue < 25 : 
+        scale = 0.1
+        isActive = True  
+   
+    elif invalue >= 2 and invalue <= 25 :
+        canvas.itemconfig(id1, fill = "red")
+        isActive = True 
+        isPressed = True
+        #needs to switch invalue in here 
+        
+    elif invalue > 25 and invalue < 35 :
+        scale = 0.1
+        isActive = True
+
+    elif invalue > 35 and invalue < 200 :
+        canvas.itemconfig(id1, fill = "red")   
+        isActive = True 
+        isPressed = True
+    
+   
+    
+   
+    default += scale 
+    scale = 0.00
+    default /= 2
+    default += 1
+
+
+   
+   
+     
+    if isActive :
+        canvas.itemconfig(id1, fill = "red")
+        canvas.scale("Circle", center[0] , center[1], default  , default )
+        default = 0.00
+        scale = 0.00
+
+    canvas.after(delay)
+    canvas.update()
+
+    
+
+    if invalue == 0 or invalue == 1 :
+        count += 1 
+        if count == breakpoint : 
+            isPressed = False
+            count = 0
+
+
+root.mainloop()
+
+
+
+def scale_set(invalue, newscale) :
+    isActive = False
+    isPassed = False # flag for being passed 
 
 
     if invalue <= 1 and isPressed == False : 
@@ -71,24 +136,23 @@ while True :
         isActive = False 
 
     elif invalue > 1 and invalue < 2 : 
-        scale = 0.1
+        scale = newscale
         isActive = True  
-         
-
+   
     elif invalue >= 2 and invalue <= 25 :
         canvas.itemconfig(id1, fill = "red")
         isActive = False 
         isPressed = True
-    
-    elif invalue > 25 and invalue < 35 :
-        scale = 0.1  
-        isActive = True
         
+    elif invalue > 25 and invalue < 35 :
+        scale = newscale
+        isActive = True
 
     elif invalue > 35 and invalue < 200 :
         canvas.itemconfig(id1, fill = "red")   
         isActive = False 
         isPressed = True
+         
     
    
     default += scale 
@@ -97,7 +161,7 @@ while True :
     default += 1
 
 
-    print(invalue)
+    # print(invalue)
    
      
     if isActive :
@@ -108,33 +172,10 @@ while True :
 
     canvas.after(delay)
     canvas.update()
-    
-    #if the count is a modulus of 5 clear circle 
 
-    
-
-    if invalue == 0 :
+    if invalue == 0 or invalue == 1 :
         count += 1 
         if count == breakpoint : 
             isPressed = False
             count = 0
-    # if count % breakpoint == 0 :
-         # canvas.delete(id1)
-         # canvas.pack()
-         # canvas.update()
-         # id1 = canvas.create_oval(x, y, x1, y1, tag = "Circle" , fill = "white", outline = "white")
-    #      canvas.after(delay)
-    #      scale = 0.00
-    #      default = 0.01
-
-
-
-
-    
-
-    
-
-
-
-
-root.mainloop()
+    return
